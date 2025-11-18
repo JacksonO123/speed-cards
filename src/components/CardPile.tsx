68c395a15f1f2ca9e63d0d05cccbe311e0a5e79f
Jackson Otto <jacksonotto120@gmail.com>
2025-11-17 23:20:18 -0800

    stuff

--- src/components/CardPile.tsx
+++ src/components/CardPile.tsx
@@ -1,6 +1,7 @@
-import { For } from "solid-js";
+import { createEffect, createSignal, For } from "solid-js";
 import type { Card as CardType, Point } from "../types/types";
 import Card from "./Card";
+import { floatDuration } from "../hooks/useFloatInstr";
 
 type CardPileProps = {
     cards: CardType[];
@@ -13,21 +14,58 @@ export default function CardPile(props: CardPileProps) {
         <div class="relative">
             <For each={props.cards}>
                 {(card, index) => (
-                    <div
-                        style={{
-                            position: index() === props.cards.length - 1 ? "relative" : "absolute",
-                            "z-index": index() === props.cards.length - 1 ? 1000 : index(),
-                            transform: `translate(${-16 * (props.cards.length - index() - 1)}px, ${16 * (props.cards.length - index() - 1)}px)`,
-                        }}
-                    >
-                        <Card
-                            card={card}
-                            width={props.width}
-                            onClick={(pos) => props.onClick?.(pos, card.id)}
-                        />
-                    </div>
+                    <PileCard
+                        card={card}
+                        index={index()}
+                        width={props.width}
+                        onClick={props.onClick}
+                        numCards={props.cards.length}
+                    />
                 )}
             </For>
+        </div>
+    );
+}
+
+type PileCardProps = {
+    index: number;
+    card: CardType;
+    numCards: number;
+    width: number;
+    onClick: (pos: Point, id: string) => void;
+};
+
+function PileCard(props: PileCardProps) {
+    const [offset, setOffset] = createSignal<Point>({ x: 0, y: 0 });
+
+    function getOffset(index: number): Point {
+        return {
+            x: -16 * (props.numCards - 1 - index),
+            y: 16 * (props.numCards - 1 - index),
+        };
+    }
+
+    createEffect(() => {
+        setOffset(getOffset(props.index));
+    });
+
+    createEffect(() => {
+        console.log(offset());
+    });
+
+    return (
+        <div
+            style={{
+                position: props.index === props.numCards - 1 ? "relative" : "absolute",
+                "z-index": props.index === props.numCards - 1 ? 1000 : props.index,
+                transform: `translate(${offset().x}px, ${offset().y}px)`,
+            }}
+        >
+            <Card
+                card={props.card}
+                width={props.width}
+                onClick={(pos) => props.onClick(pos, props.card.id)}
+            />
         </div>
     );
 }
